5dcd6d48f12c5e7a62a5b2f5362647a0851b45ff
aniket-6234 <aniketashok1999@gmail.com>
2022-03-26 10:36:08 +0530

    perfect-fanclub

--- src/components/CreatorProfile/GamerzFanclub.js
+++ src/components/CreatorProfile/GamerzFanclub.js
@@ -33,24 +33,32 @@ const GamerzFanclub = () => {
           )))}
            </div>
 
-          {seeMore ? 
-              <div className="w-full lg:w-4/5 lg:-mt-4 flex flex-wrap justify-evenly sm:justify-around">
-              {array.slice(0, 5).map((arr => (
+           <div className="mb-12 w-4/5 lg:w-3/5 mt-2 lg:-mt-4 flex flex-wrap lg:justify-between justify-around">
+              {array.slice(0, 4).map((arr => (
                <div key={array} className="w-12 h-12 lg:w-36 lg:h-36 lg:mt-4 rounded-full border-2 border-pink-600 bg-gray-500">
+                 <img src={FanClubImg} alt="fanClubImg" />
+               </div>
+             )))}
+            </div>
+
+          {seeMore ? 
+              <div className="w-full -mt-4 lg:w-4/5 lg:-mt-4 flex flex-wrap justify-evenly sm:justify-around">
+              {array.slice(0, 6).map((arr => (
+               <div key={array} className="opacity-30 w-12 h-12 lg:w-28 lg:h-28 lg:mt-4 rounded-full border-2 border-pink-600 bg-gray-500">
                  <img src={FanClubImg} alt="fanClubImg" />
                </div>
              )))}
               </div>
               :
-              <div className=" w-4/5 -mt-4 flex flex-wrap justify-evenly">
+              <div className="w-full -mt-12 lg:w-4/5 flex flex-wrap justify-around lg:justify-evenly">
               {array.slice(5, array.length).map((arr => (
-                <div key={array} className="w-12 h-12 lg:w-36 lg:h-36 mt-4 lg:mt-12 rounded-full border-2 border-pink-600 bg-gray-500">
+                <div key={array} className="w-12 h-12 mt-8 lg:w-28 ml-2 mr-2 lg:ml-4 lg:mr-4 lg:h-28 lg:mt-12 rounded-full border-2 border-pink-600 bg-gray-500">
                   <img src={FanClubImg} alt="fanClubImg" />
                 </div>
               )))}
               </div>  
           }
-           <div onClick={() => showMore()} className="w-40 h-12 mb-16 mt-12 flex justify-evenly items-center cursor-pointer rounded-3xl border-2 text-white">{seeMore ? "see more" : "see less"}</div>
+           <div onClick={() => showMore()} className="w-40 h-12 mb-16 mt-12 flex justify-evenly items-center cursor-pointer rounded-3xl border-2 text-white">{seeMore ? "Show More" : "Show Less"}</div>
        </div>
        <div className="h-20"></div>
        <EmptyFanclub />
